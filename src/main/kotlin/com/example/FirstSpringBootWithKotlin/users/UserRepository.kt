7b8aefa4a028dedfcadd90c8bff759d8d901dcb4
JPedro759 <joaopedromeloo03@gmail.com>
2025-07-21 22:07:31 -0300

    Removing unnecessary method.

--- src/main/kotlin/com/example/FirstSpringBootWithKotlin/users/UserRepository.kt
+++ src/main/kotlin/com/example/FirstSpringBootWithKotlin/users/UserRepository.kt
@@ -3,7 +3,4 @@ package com.example.FirstSpringBootWithKotlin.users
 import org.springframework.data.repository.CrudRepository
 import java.util.Optional
 
-interface UserRepository : CrudRepository<User, Long> {
-
-    override fun findById(id: Long): Optional<User>
-}
+interface UserRepository : CrudRepository<User, Long>
